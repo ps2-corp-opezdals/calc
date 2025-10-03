76db1ca3fc0d2a408f83a316b9289b6033874a77
_Bor4iK_ <shafiev06@mail.ru>
2025-10-03 16:45:10 +0500

    upd

--- calc.cpp
+++ calc.cpp
@@ -1,2 +1,15 @@
 #include "bibls.h"
 
+class Calculator {
+    private:
+        unsigned int year;
+        std::string name;
+        unsigned int model;
+    public:
+        Calculator(unsigned int y , unsigned int m , std::string n ){
+            year = y;
+            model = m;
+            name = n;
+        }
+         
+};
